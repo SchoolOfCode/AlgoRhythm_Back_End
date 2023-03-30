46559fd671e9ab499975ee360f719bb4824e4cd0
GabrielFdeOliveira <gbl.diesel@hotmail.com>
2023-03-30 11:23:42 +0100

    Some adjustments to the app file

--- App.js
+++ App.js
@@ -1,13 +1,22 @@
 import express from "express";
 import cors from "cors";
+import morgan from "morgan";
+import cors from "cors";
 
-const App = express();
-App.use(cors());
+const app = express();
 
 const PORT = process.env.PORT;
 
-App.listen(PORT, () => {
-    console.log("I'm Listening");
-  });
+app.use(morgan("dev"));
 
-  export default App;
+app.use(cors());
+
+app.use(express.json());
+
+app.use("/api/candidates", router);
+
+app.listen(PORT, () => {
+  console.log(`I'm Listening on port ${PORT}`);
+});
+
+export default App;
